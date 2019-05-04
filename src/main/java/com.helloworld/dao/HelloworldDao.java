47c12c6549d07409a90d0b9cb58a5402711a26d1
秦斌 <13475457579@163.com>
2019-05-04 17:34:42 +0800

    update

--- src/main/java/com.helloworld/dao/HelloworldDao.java
+++
@@ -1,9 +0,0 @@
-package com.helloworld.dao;
-
-/**
- * Created by 11023 on 2018/3/4.
- */
-public interface HelloworldDao {
-
-    public String getHelloworld();
-}
--- src/main/java/com.helloworld/service/HelloworldService.java
+++
@@ -1,9 +0,0 @@
-package com.helloworld.service;
-
-/**
- * Created by 11023 on 2018/3/4.
- */
-public interface HelloworldService {
-
-    public String getHelloworld();
-}
